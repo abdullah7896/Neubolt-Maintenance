d88a07d00c6295dc3c1922939672afc1bb4627a4
Haroon Anwar <142000850+Haroon527@users.noreply.github.com>
2026-01-14 11:35:51 +0500

    Add apiUrl to production environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   appVersion: packageInfo.version,
-  production: true
+  production: true,
+  apiUrl: 'http://72.62.183.8/neubolt'
 };
